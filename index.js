925406c3ea5785e6fe4a1c6520b18df486350a86
pixies <pixies@riseup.net>
2019-10-29 09:25:52 -0300

    adcionado funcao que criar os elemento ol

---
+++ index.js
@@ -0,0 +1,5 @@
+function elementoOl() {
+    var nova_ol = document.createElement('ol');
+    nova_ol.setAttribute('id','lista-ordenada');
+    document.getElementById('principal').appendChild(nova_ol);
+}
